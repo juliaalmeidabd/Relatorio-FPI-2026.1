494d6158a630b0586ee089a4a961239c7b1785ee
Júlia Almeida <juliaabdantas@gmail.com>
2026-03-18 08:35:39 -0300

    Add pyramid pattern printing based on character input
    This program prints a pyramid pattern based on the input character, with each row displaying letters in ascending and descending order.

---
+++ Lista 2 - Laços de repetição/Quest1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    char entrada;
+
+    if (scanf("%c", &entrada) != 1) return 0;
+
+    int n = entrada - 'A' + 1; //quantidade de linhas da pirâmide
+
+    for (int i = 0; i < n; i++) {
+        char letra_linha = 'A' + i;
+    
+
+        for (int j = 0; j < (entrada - letra_linha); j++) { //pontos esquerda
+            printf(".");
+        }
+
+        //sequ�ncia de letras at� a letra da linha
+        for (char sequencia_letras = 'A'; sequencia_letras <= letra_linha; sequencia_letras++) {
+            printf("%c", sequencia_letras);
+        }
+
+        //continua a sequência, com as letras em ordem decrescente até a A
+        for (char sequencia_letras = letra_linha - 1; sequencia_letras >= 'A'; sequencia_letras--) {
+            printf("%c", sequencia_letras);
+        }
+
+        for (int j = 0; j < (entrada - letra_linha); j++) { //pontos direita
+            printf(".");
+        }
+
+        printf("\n");
+    }
+    
+
+    return 0;
+}
